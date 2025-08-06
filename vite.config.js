adfed2cbc2c73ab9ee01935c214a34a0525e2b9d
MIMUHAMMADU <M.I.Muhammadribah@gmail.com>
2025-08-06 15:40:21 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
+// vite.config.js
 export default defineConfig({
+  base: "/tmdb-movies-app/",
   plugins: [react()],
-  server: {
-    port: 5173,
-  },
-})
+});
