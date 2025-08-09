43b1692cf6e191581cf06c474cc782f73709b23f
MIMUHAMMADU <M.I.Muhammadribah@gmail.com>
2025-08-09 15:23:15 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Edit wannan 'base' zuwa sunan repo ɗinka
 export default defineConfig({
   plugins: [react()],
   base: '/tmdb-movies-app/'
